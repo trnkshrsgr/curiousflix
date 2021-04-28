2d9a03180e833cda16104ca1e4830d6fe8c1568d
trnkshrsgr <83308405+trnkshrsgr@users.noreply.github.com>
2021-04-28 09:09:46 +0530

    Add files via upload

---
+++ g00d.php
@@ -0,0 +1,33 @@
+<DOCTYPEhtml!>
+<html>
+
+
+<head>
+<style>
+  body{ 
+  background-color:orange; 
+    }
+	h2{color:blue;}
+	div{background-color:red;
+	}
+</style>
+</head>
+<body>
+<div><h2 style="color:violet;"><center><I>THIS IS SO COOOL </center><I></h2>
+<br>
+<hr>
+</div>
+<input></input>
+<input></input>
+<img src="C:\Users\user\Pictures\my fav paint.png" width="300" height="200">
+
+<center><audio controls>
+<source src="C:\Users\Public\Music\Sample Music\Received\k391.mp3" type="audio/mpeg">
+ </audio> </center><hr><center>
+ <video width="300" height="400" autoplay fullscreen controls> 
+<source src="C:\Users\user\Downloads\avg.mp4" type="video/mp4"> 
+ 
+</video>
+</center>
+</body>
+</html> 
